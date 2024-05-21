01de65c7a3e8f65b52dffceb0404273cec6466f1
seomini <seomini970204@gmail.com>
2024-05-21 10:34:17 +0900

    Initial Commit

---
+++ qriz/qrizApp.swift
@@ -0,0 +1,17 @@
+//
+//  qrizApp.swift
+//  qriz
+//
+//  Created by mimi_0_0 on 2024/05/21.
+//
+
+import SwiftUI
+
+@main
+struct qrizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
