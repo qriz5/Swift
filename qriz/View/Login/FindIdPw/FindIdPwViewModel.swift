3491264390686cb34de0ac7150e60ac470a5814c
seomini <seomini970204@gmail.com>
2024-05-27 14:08:02 +0900

    아이디/비밀번호 찾기 모델 텝 수정

--- qriz/View/Login/FindIdPw/FindIdPwViewModel.swift
+++ qriz/View/Login/FindIdPw/FindIdPwViewModel.swift
@@ -8,14 +8,9 @@
 import Foundation
 
 class FindIdPwViewModel: ObservableObject {
-    @Published var selectedTab: Tab = .findId
     @Published var name: String = ""
     @Published var id: String = ""
     @Published var email: String = ""
     @Published var verificationCode: String = ""
     
-    enum Tab: String, CaseIterable {
-        case findId = "아이디 찾기"
-        case findPw = "비밀번호 찾기"
-    }
 }
