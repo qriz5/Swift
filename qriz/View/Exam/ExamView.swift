148df14022ca64d2aaffbbce4225f4d765dfb0e2
seomini <seomini970204@gmail.com>
2024-05-28 12:55:27 +0900

    Tab 모델 생성

---
+++ qriz/View/Exam/ExamView.swift
@@ -0,0 +1,18 @@
+//
+//  ExamView.swift
+//  qriz
+//
+//  Created by mimi_0_0 on 2024/05/28.
+//
+
+import SwiftUI
+
+struct ExamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExamView()
+}
---
+++ qriz/View/MyPage/MyPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageView.swift
+//  qriz
+//
+//  Created by mimi_0_0 on 2024/05/28.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPageView()
+}
