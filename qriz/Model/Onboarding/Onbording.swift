7351e6e408fc11bfd348bf38de13400beb21eed9
seomini <seomini970204@gmail.com>
2024-05-28 11:36:26 +0900

    온보딩 모델 생성

--- qriz/Model/Onboarding/Onbording.swift
+++ qriz/Model/Onboarding/Onbording.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct SelectableBox: Hashable, Identifiable {
+    var id = UUID() 
+    var title: String
+    var isSelected: Bool = false
+
+}
+//struct SelectableBox: Hashable, Equatable {
+//    var title: String
+//    var isSelected: Bool
+//    var action: () -> Void
+//    
+//    func hash(into hasher: inout Hasher) {
+//        hasher.combine(title)
+//        hasher.combine(isSelected)
+//    }
+//    
+//    static func == (lhs: SelectableBox, rhs: SelectableBox) -> Bool {
+//        return lhs.title == rhs.title && lhs.isSelected == rhs.isSelected
+//    }
+//}
