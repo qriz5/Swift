8b2d3fe65ffa18f2fab32d50fab4e560332172ce
seomini <seomini970204@gmail.com>
2024-05-29 15:29:47 +0900

    settingView 생성

--- qriz/View/Setting/SettingView.swift
+++ qriz/View/Setting/SettingView.swift
@@ -7,12 +7,109 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct SettingView: View {
+    @StateObject private var settingViewModel = SettingViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                CustomNavigationBar(
+                    leftBtnAction: {
+//                        pathModel.paths.removeLast()
+                    }
+                )
+                
+                Spacer()
+                
+                profileSettingView(settingViewModel: settingViewModel)
+                settingSettingView()
+                
+                Spacer().frame(height: 250)
+            }
+        }
     }
 }
 
-#Preview {
-    SettingView()
+struct profileSettingView : View {
+    @ObservedObject var settingViewModel: SettingViewModel
+    
+    var body: some View{
+        VStack(alignment: .leading, spacing: 20) {
+            
+            Text("프로필")
+                .font(.system(size: 16))
+                .foregroundColor(.gray)
+            
+            Spacer().frame(height: 10)
+            
+            Text("이름")
+                .font(.system(size: 22))
+                .foregroundColor(.black)
+            TextField("이름 입력", text: $settingViewModel.name)
+                .disabled(true)
+            Text("이메일")
+                .font(.system(size: 22))
+                .foregroundColor(.black)
+            TextField("이메일 입력", text: $settingViewModel.email)
+                .disabled(true)
+        }
+        .padding()
+    }
 }
+
+struct settingSettingView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text("설정")
+                .font(.system(size: 16))
+                .foregroundColor(.gray)
+            
+            Spacer().frame(height: 5)
+            
+            VStack() {
+                Button(action: {
+                    print("로그아웃 버튼 클릭됨")
+                    // 로그아웃 액션 추가
+                }) {
+                    HStack {
+                        Text("로그아웃")
+                            .font(.system(size: 22))
+                            .foregroundColor(.black)
+                        Spacer()
+                    }
+                    .background(Color.white)
+                }
+                .buttonStyle(PlainButtonStyle())
+                
+                Spacer().frame(height: 35)
+                
+                Button(action: {
+                    print("계정 탈퇴 버튼 클릭됨")
+                    // 계정 탈퇴 액션 추가
+                }) {
+                    HStack {
+                        Text("계정 탈퇴")
+                            .font(.system(size: 22))
+                            .foregroundColor(.black)
+                        Spacer()
+                    }
+                    .background(Color.white)
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
+
