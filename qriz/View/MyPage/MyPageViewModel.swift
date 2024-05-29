11f8ee401b06875e8685c3e7ffc01a0d9de54f55
seomini <seomini970204@gmail.com>
2024-05-29 15:29:14 +0900

    마이페이지 뷰 모델 수정

--- qriz/View/MyPage/MyPageViewModel.swift
+++ qriz/View/MyPage/MyPageViewModel.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+class MyPageViewModel: ObservableObject {
+    @Published var services = [
+        "FAQ",
+        "서비스 이용약관",
+        "개인정보처리 방침",
+        "버전정보",
+        "공지사항"
+    ]
+}
