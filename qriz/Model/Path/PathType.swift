144b81b2cdc0e45f4e5df92c8f94d167c3f0e811
seomini <seomini970204@gmail.com>
2024-05-31 15:43:28 +0900

    PathType 추가 id/pw 분리 및 회원 가입 화면 분할

--- qriz/Model/Path/PathType.swift
+++ qriz/Model/Path/PathType.swift
@@ -2,7 +2,34 @@
 //  PathType.swift
 //  qriz
 //
-//  Created by mimi_0_0 on 2024/05/23.
-//
 
 import Foundation
+
+enum PathType: Hashable {
+    case signUpView
+    case findIdView
+    case findPwView
+}
+
+//회원가입 화면
+enum SignUpStep {
+    case name, email, id, password
+    
+    func next() -> SignUpStep {
+        switch self {
+        case .name: return .email
+        case .email: return .id
+        case .id: return .password
+        case .password: return .name
+        }
+    }
+    
+    func previous() -> SignUpStep {
+        switch self {
+        case .name: return .name
+        case .email: return .name
+        case .id: return .email
+        case .password: return .id
+        }
+    }
+}
