e2b9077814cdf6641e49b3dd9276cf3e5515ee43
seomini <seomini970204@gmail.com>
2024-05-31 15:42:35 +0900

    Component 메일 발송 버튼 생성

--- qriz/Component/SendEmailButton/SendEmailButton.swift
+++ qriz/Component/SendEmailButton/SendEmailButton.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct SendEmailButton: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Spacer()
+        
+                Button(action: {
+                    // 이메일 발송
+        
+                    print("이메일 발송 버튼 클릭됨")
+                }) {
+                    Text("이메일 발송")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.gray)
+                        .cornerRadius(5.0)
+                }
+                .padding(.horizontal)
+                
+                Spacer().frame(height: 10)
     }
 }
 
