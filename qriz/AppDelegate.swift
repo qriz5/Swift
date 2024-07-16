b6ce5ba77399b949e8458ea352447d83e80b13c3
seomini <seomini970204@gmail.com>
2024-07-16 13:42:24 +0900

    AppDelegate 수정

--- qriz/AppDelegate.swift
+++ qriz/AppDelegate.swift
@@ -5,4 +5,21 @@
 //  Created by mimi_0_0 on 2024/06/06.
 //
 
-import Foundation
+import UIKit
+import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+    
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
+    }
+}
--- qriz/View/Conceptbook/Conceptbookdetail/ConceptBookMain/ConceptBookMainView.swift
+++ qriz/View/Conceptbook/Conceptbookdetail/ConceptBookMain/ConceptBookMainView.swift
@@ -6,13 +6,51 @@
 //
 
 import SwiftUI
+import PDFKit
 
 struct ConceptBookMainView: View {
+    @ObservedObject var conceptBookMainViewModel: ConceptBookMainViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TitleConceptBookView(topic: conceptBookMainViewModel.topic, subjectType: conceptBookMainViewModel.subjectType)
+            
+            PDFKitView(pdfURL: conceptBookMainViewModel.pdfURL)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.gray)
+            
+        }
+        .navigationTitle("개념서 상세")
     }
 }
 
-#Preview {
-    ConceptBookMainView()
+// MARK: - TitleConceptBookView
+struct TitleConceptBookView: View {
+    let topic: String
+    let subjectType: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack{
+                VStack(alignment: .leading) {
+                    Text(subjectType.contains("SQL") ? "2과목" : "1과목")
+                        .font(.system(size: 16))
+                    Spacer().frame(height: 5)
+                    Text(topic)
+                        .font(.system(size: 16))
+                }
+                Spacer()
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color(.systemGray5))
+        }
+    }
+}
+
+// MARK: - Preview
+struct ConceptBookMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConceptBookMainView(conceptBookMainViewModel: ConceptBookMainViewModel(topic: "데이터모델의 이해", subjectType: "1과목"))
+    }
 }
