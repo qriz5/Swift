0fd98f90a166755bc5114c0eba2628f68e7401fc
seomini <seomini970204@gmail.com>
2024-08-02 10:34:22 +0900

    BackButton 컴포넌트 생성

---
+++ qriz/Component/BackButton/BackButton.swift
@@ -0,0 +1,18 @@
+//
+//  BackButton.swift
+//  qriz
+//
+//  Created by mimi_0_0 on 2024/08/01.
+//
+
+import SwiftUI
+
+struct BackButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackButton()
+}
