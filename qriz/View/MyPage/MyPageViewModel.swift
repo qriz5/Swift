e8be9fd87b9fe9675f9f206aa8430b4cdefda652
seomini <seomini970204@gmail.com>
2024-08-06 16:14:12 +0900

    마이페이지 수정

--- qriz/View/MyPage/MyPageViewModel.swift
+++ qriz/View/MyPage/MyPageViewModel.swift
@@ -8,11 +8,5 @@
 import Foundation
 
 class MyPageViewModel: ObservableObject {
-    @Published var services = [
-        "FAQ",
-        "서비스 이용약관",
-        "개인정보처리 방침",
-        "버전정보",
-        "공지사항"
-    ]
+
 }
--- qriz/View/TestDaily/TestDailyView.swift
+++ qriz/View/TestDaily/TestDailyView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct TestDailyView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            CustomNavigationBar(
+                isDisplayLeftBtn: false,
+                isDisplayRightBtn: false,
+                isCenterTitle: true,
+                centerTitleType: .dailystudy
+            )
+            TestDailyTitle()
+            DailyButtonView()
+            Spacer().frame(height: 10)
+            DailyTestView()
+            Spacer().frame(height: 150)
+        }
+        .padding()
+        .background(Color.gray.opacity(0.1))
+    }
+}
+//MARK: titleView
+struct TestDailyTitle: View {
+    var body: some View{
+        VStack(alignment: .leading, spacing: 10) {
+            Text("관련된 개념 보러가기")
+                .font(.system(size: 20, weight: .bold))
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        
     }
 }
 
