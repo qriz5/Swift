b4a21d242ea9f2060fd32f1303d151b9d7007351
seomini <seomini970204@gmail.com>
2024-08-06 16:15:05 +0900

    세팅화면 수정

--- qriz/View/Setting/SettingView.swift
+++ qriz/View/Setting/SettingView.swift
@@ -14,94 +14,144 @@ struct SettingView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Spacer()
-                
-                CustomNavigationBar(
-                    leftBtnAction: {
-//                        pathModel.paths.removeLast()
+            NavigationStack {
+                ZStack {
+                    Color.customBackground.edgesIgnoringSafeArea(.all)
+                    VStack {
+        
+                        profileSettingView(settingViewModel: settingViewModel)
+                        settingSettingView()
+                        Spacer()
                     }
-                )
-                
-                Spacer()
-                
-                profileSettingView(settingViewModel: settingViewModel)
-                settingSettingView()
-                
-                Spacer().frame(height: 250)
+                }
+            }
+            .navigationTitle("설정")
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    BackButton()
+                }
             }
         }
     }
 }
-
 struct profileSettingView : View {
     @ObservedObject var settingViewModel: SettingViewModel
-    
+    let emailText = "seomini@naver.com"
     var body: some View{
-        VStack(alignment: .leading, spacing: 20) {
+        HStack{
+            Spacer().frame(width: 10)
+            VStack(alignment: .leading, spacing: 20) {
+                Text("서민이님")
+                    .font(.system(size: 22, weight: .bold))
+                    .foregroundColor(.black)
+                HStack{
+                    Text("") +
+                    Text(emailText)
+                        .font(.system(size: 18))
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding(.horizontal)
             
-            Text("프로필")
-                .font(.system(size: 16))
-                .foregroundColor(.gray)
-            
-            Spacer().frame(height: 10)
-            
-            Text("이름")
-                .font(.system(size: 22))
-                .foregroundColor(.black)
-            TextField("이름 입력", text: $settingViewModel.name)
-                .disabled(true)
-            Text("이메일")
-                .font(.system(size: 22))
-                .foregroundColor(.black)
-            TextField("이메일 입력", text: $settingViewModel.email)
-                .disabled(true)
+            Spacer()
         }
-        .padding()
     }
 }
 
 struct settingSettingView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Text("설정")
-                .font(.system(size: 16))
-                .foregroundColor(.gray)
-            
-            Spacer().frame(height: 5)
-            
-            VStack() {
-                Button(action: {
-                    print("로그아웃 버튼 클릭됨")
-                    // 로그아웃 액션 추가
-                }) {
-                    HStack {
-                        Text("로그아웃")
-                            .font(.system(size: 22))
-                            .foregroundColor(.black)
-                        Spacer()
-                    }
-                    .background(Color.white)
-                }
-                .buttonStyle(PlainButtonStyle())
-                
-                Spacer().frame(height: 35)
-                
-                Button(action: {
-                    print("계정 탈퇴 버튼 클릭됨")
-                    // 계정 탈퇴 액션 추가
-                }) {
-                    HStack {
-                        Text("계정 탈퇴")
-                            .font(.system(size: 22))
-                            .foregroundColor(.black)
-                        Spacer()
-                    }
-                    .background(Color.white)
-                }
+        VStack(alignment: .leading, spacing: 10) {
+                                                            
+            HStack {
+                Text("회원정보 수정")
+                    .font(.body)
+                Spacer()
+                Image("Property")
+                    .resizable()
+                    .frame(width: 24, height: 24)
             }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(8)
+            .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
+            
+            HStack {
+                Text("프로필 수정")
+                    .font(.body)
+                Spacer()
+                Image("Property")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+            }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(8)
+            .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
+            
+            NavigationLink(destination: ChangePwView()) {
+                HStack {
+                    Text("비밀번호 변경")
+                        .font(.body)
+                    Spacer()
+                    Image("Property")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(8)
+                .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
+            }
+            .buttonStyle(PlainButtonStyle()) 
+
+            
+            
+            HStack {
+                Text("알람 설정")
+                    .font(.body)
+                Spacer()
+                Image("Property")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+            }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(8)
+            .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
+            
+            
+            HStack {
+                Text("로그아웃")
+                    .font(.body)
+                Spacer()
+                Image("Property")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+            }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(8)
+            .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
+            
+            
+            HStack {
+                Text("계정 탈퇴 버튼 클릭됨")
+                    .font(.body)
+                Spacer()
+                Image("Property")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+            }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(8)
+            .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
         }
-        .padding()
+        .padding(20)
+    
+
+
     }
 }
 
