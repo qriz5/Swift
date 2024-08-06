44a9a435a4cb2afed3bf9c095a057d7a6ba3e60c
seomini <seomini970204@gmail.com>
2024-08-06 16:13:42 +0900

    로그인 뷰모델 수정

--- qriz/View/Login/LoginViewModel.swift
+++ qriz/View/Login/LoginViewModel.swift
@@ -6,12 +6,26 @@
 //
 
 import Foundation
+import Combine
 
 class LoginViewModel: ObservableObject {
     @Published var username: String = ""
     @Published var password: String = ""
-    
+    @Published var showAlert: Bool = false
+    @Published var alertMessage: String = ""
+
+
     func login() {
-        print("로그인 시도: 사용자 이름 - \(username), 비밀번호 - \(password)")
+        if username.isEmpty || password.isEmpty {
+            alertMessage = "아이디와 비밀번호를 입력해 주세요."
+            showAlert = true
+        } else if username == "testuser" && password == "password123" {
+            alertMessage = "로그인 성공!"
+            showAlert = true
+        } else {
+            // Simulate a failed login
+            alertMessage = "로그인 실패: 아이디 혹은 비밀번호가 잘못되었습니다."
+            showAlert = true
+        }
     }
 }
