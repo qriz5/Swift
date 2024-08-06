15b80c706ea33a6b013623cd5d04b6d03b88b0fd
seomini <seomini970204@gmail.com>
2024-08-06 16:10:07 +0900

    텝버튼 생성

--- qriz/Component/TabButton/TabButton.swift
+++ qriz/Component/TabButton/TabButton.swift
@@ -18,44 +18,53 @@ struct TabButton: View {
                 selectedTab = title
             }) {
                 Text(title)
-                    .foregroundColor(selectedTab == title ? .black : .gray)
+                    .foregroundColor(selectedTab == title ? .blue : Color.customProgressBarTC)
             }
             .padding(.vertical, 8)
             
             if selectedTab == title {
                 Rectangle()
                     .frame(height: 2)
-                    .foregroundColor(.black)
+                    .foregroundColor(.blue)
             } else {
                 Rectangle()
                     .frame(height: 2)
-                    .foregroundColor(.gray)
+                    .foregroundColor(.clear)
             }
+            
         }
     }
 }
 
 struct FilterButton: View {
     let title: String
-    var isToggle: Bool = false
-    @State private var isSelected = false
-    
+    var imageName: String?
+    @Binding var isSelected: Bool
+
     var body: some View {
         Button(action: {
-            if isToggle {
-                isSelected.toggle()
-            }
+            isSelected.toggle()
         }) {
-            Text(title)
-                .padding(.horizontal, 10)
-                .padding(.vertical, 5)
-                .background(isToggle && isSelected ? Color.gray.opacity(0.2) : Color.clear)
-                .cornerRadius(8)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(Color.gray, lineWidth: 1)
-                )
+            HStack(spacing: 0) {
+                if let imageName = imageName, !imageName.isEmpty {
+                    Image(imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20)
+                }
+                Text(title)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .foregroundColor(isSelected ? .blue : .black)
+            }
+            .padding(1)
+            .frame(height: 40)
+            .background(isSelected ? Color.white : Color(red: 240/255, green: 244/255, blue: 247/255))
+            .cornerRadius(8)
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(isSelected ? Color.blue : .clear)
+            )
         }
-        .foregroundColor(.black)
     }
 }
