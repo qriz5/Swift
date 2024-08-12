85ffc4bfde0b479fc1cea06fd96bec87c7eddb0f
seomini <seomini970204@gmail.com>
2024-08-12 11:52:56 +0900

    시간 모듈 생성

--- qriz/Extension/Date.swift
+++ qriz/Extension/Date.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+extension Date {
+    
+}
+
+extension JSONDecoder {
+    static var customDecoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(DateFormatter.iso8601Full)
+        return decoder
+    }
+}
+
+extension DateFormatter {
+    static let iso8601Full: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
+    }()
+}
